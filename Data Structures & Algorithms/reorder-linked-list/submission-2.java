5fbf5b94c9649cad3176b5eba1086e92392ebb69
DivyanshiKesarwani <98300398+DivyanshiKesarwani@users.noreply.github.com>
2026-05-21 12:39:36 +0530

    Add: reorder-linked-list - submission-2

---
+++ Data Structures & Algorithms/reorder-linked-list/submission-2.java
@@ -0,0 +1,53 @@
+class Solution {
+    public void reorderList(ListNode head) {
+        if (head == null || head.next == null) return;
+
+        ListNode prevMid = middle(head);//first half last node 
+
+        ListNode second = prevMid.next;//second half start
+        prevMid.next = null;
+
+        second = reverse(second);
+
+        ListNode first = head;
+
+        while(second!=null)
+        {
+            ListNode firstNext=first.next;
+            ListNode secondNext=second.next;
+
+            first.next=second;
+            second.next=firstNext;
+
+            first=firstNext;
+            second=secondNext;
+        }
+    }
+
+    public ListNode reverse(ListNode head) {
+        ListNode prev = null;
+        ListNode temp = head;
+
+        while (temp != null) {
+            ListNode front = temp.next;
+            temp.next = prev;
+            prev = temp;
+            temp = front;
+        }
+
+        return prev;
+    }
+
+//this function leads the middle to point to first half last node or prev to middle
+    public ListNode middle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return slow;
+    }
+}
