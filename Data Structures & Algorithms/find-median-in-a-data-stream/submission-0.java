18c12e04b5287d4cc9cc810e3c142f442dc6c9fa
DivyanshiKesarwani <98300398+DivyanshiKesarwani@users.noreply.github.com>
2026-07-05 17:40:42 +0530

    Add: find-median-in-a-data-stream - submission-0

---
+++ Data Structures & Algorithms/find-median-in-a-data-stream/submission-0.java
@@ -0,0 +1,38 @@
+class MedianFinder {
+    PriorityQueue<Integer>min;
+    PriorityQueue<Integer>max;
+    public MedianFinder() {
+        this.min=new PriorityQueue<>();
+        this.max=new PriorityQueue<>((a,b)->(b-a));
+    }
+    
+    public void addNum(int num) {
+        if(max.isEmpty()||num<=max.peek())
+        {
+            max.add(num);
+        }
+        else{
+            min.add(num);
+        }
+        if(max.size()>min.size()+1)
+        {
+            min.add(max.poll());
+        }
+        else if(min.size()>max.size()+1)
+        {
+            max.add(min.poll());
+        }
+    }
+    
+    public double findMedian() {
+        if (max.size() > min.size()) {
+        return max.peek();
+    }
+
+    if (min.size() > max.size()) {
+        return min.peek();
+    }
+
+    return (max.peek() + min.peek()) / 2.0;
+        }
+}
