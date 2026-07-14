1f0c063d38d467dcdff53270f905322ef7289138
DivyanshiKesarwani <98300398+DivyanshiKesarwani@users.noreply.github.com>
2026-07-14 23:34:21 +0530

    Add: subsets - submission-1

---
+++ Data Structures & Algorithms/subsets/submission-1.java
@@ -0,0 +1,34 @@
+class Solution {
+    //Time Complexity: O(n × 2ⁿ)
+    //Time complexity = (Number of answers) × (Cost to copy one answer).
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> curr = new ArrayList<>();
+
+        generateSubsets(nums, 0, curr, ans);
+
+        return ans;
+    }
+
+    public void generateSubsets(
+            int[] nums,
+            int idx,
+            List<Integer> curr,
+            List<List<Integer>> ans) {
+
+        if (idx == nums.length) {
+            ans.add(new ArrayList<>(curr));
+            return;
+        }
+
+        // Include nums[idx]
+        curr.add(nums[idx]);
+        generateSubsets(nums, idx + 1, curr, ans);
+
+        // Backtrack
+        curr.remove(curr.size() - 1);
+
+        // Do not include nums[idx]
+        generateSubsets(nums, idx + 1, curr, ans);
+    }
+}
