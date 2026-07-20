ca3fe13f0533cde67057f68bc1807a74441cc2f8
DivyanshiKesarwani <98300398+DivyanshiKesarwani@users.noreply.github.com>
2026-07-20 12:31:39 +0530

    Add: palindrome-partitioning - submission-1

---
+++ Data Structures & Algorithms/palindrome-partitioning/submission-1.java
@@ -0,0 +1,40 @@
+class Solution {
+    public List<List<String>> partition(String s) {
+        List<List<String>>ans=new ArrayList<>();
+        ArrayList<String>curr=new ArrayList<>();
+        backtrack(s,0,curr,ans);
+        return ans;
+    }
+    public void backtrack(String s,int idx,ArrayList<String>curr,List<List<String>>ans)
+    {
+        if(idx==s.length())
+        {
+            ans.add(new ArrayList<>(curr));
+            return;
+        }
+        for(int i=idx;i<s.length();i++)
+        {
+            if(isPalindrome(s, idx, i))
+            {
+                curr.add(s.substring(idx, i + 1));
+
+                backtrack(s, i + 1, curr, ans);
+
+                curr.remove(curr.size() - 1);
+            }
+        }
+    }
+    public boolean isPalindrome(String s, int left, int right) {
+
+    while(left<right)
+    {
+        if(s.charAt(left)!=s.charAt(right))
+        {
+            return false;
+        }
+        left++;
+        right--;
+    }
+    return true;
+}
+}
